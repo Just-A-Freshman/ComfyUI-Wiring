70955929ebe40e0c865989374f41ebea8162fc7d
Chorgri <155750677+Just-A-Freshman@users.noreply.github.com>
2025-09-07 11:58:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
 import json
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Dict, Any
 
 
 from flask import Flask, Response, render_template, jsonify, send_file, request
 from werkzeug.utils import secure_filename
 
 
-from core.parser import WorkflowReader, WorkflowWriter
-from core.Utils import NodeOptions
-from core.core import LogicalConfig, CoordinateConfig
-
+from core.parser import WorkflowIO, WorkflowReader, WorkflowWriter
+from core.setting import NodeOptions
+from core.core import StandardOrder, MainBranchShader
+from core.pos_caculate import NodePosCalculator, GroupPosCalulator
 
 
 current_dir = Path(__file__).parent.absolute()
@@ -21,7 +21,7 @@ app = Flask(
     static_folder = current_dir / "web" / "static"
 )
 app.config['temp'] = Path('temp')
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 os.makedirs(app.config['temp'], exist_ok=True)
 
 
@@ -42,29 +42,38 @@ def generate_layout() -> tuple[Response, Literal[400]] | Response | tuple[Respon
     filename = secure_filename(temp_filename)
     input_path = app.config['temp'] / filename
     file.save(input_path)
-    config: dict = json.loads(request.form.get('config', '{}'))
+    config: Dict[str, Any] = json.loads(request.form.get('config', '{}'))
     NodeOptions.update_setting({
         'gap_x': config.get('gap_x', 100),
         'gap_y': config.get('gap_y', 100),
-        'max_span': config.get('max_span', 6),
-        'size_align': config.get('size_align', True),
-        'set_node': config.get('set_node', True),
-        'force_unfold': config.get('force_unfold', True)
+        'same_column_stacking_strength': config.get('same_column_stacking_strength', 1),
+        'layout_calculator': config.get('layout_calculator', 'highly_align'),
+        'remove_intermediate_nodes': config.get('remove_intermediate_nodes', True),
+        'set_color_for_main_path': config.get('set_color_for_main_path', True)
     })
-    
     try:
-        workflow_reader = WorkflowReader(input_path)
-        workflow_writer = WorkflowWriter(workflow_reader.workflow_data)
-        
-        logical_config = LogicalConfig(workflow_reader)
-        coordinate_config = CoordinateConfig(workflow_reader)
-        
-        columns = logical_config.get_logic_config()
-        coordinate_config.modify_layout(columns)
-        output_filename = f"output.json"
+        workflow_data = WorkflowIO.import_file(input_path)
+        workflow_reader = WorkflowReader(workflow_data)
+        workflow_writer = WorkflowWriter(workflow_data)
+        standard_order = StandardOrder(workflow_reader)
+        main_branch_shader = MainBranchShader(workflow_reader)
+        group_caculator = GroupPosCalulator(workflow_reader)
+        pos_caculator = NodePosCalculator(workflow_reader)
+        orig_groups = group_caculator.get_orig_groups()
+        if NodeOptions.remove_intermediate_nodes:
+            workflow_writer.remove_unnecessary_nodes()
+        columns = standard_order.get_logic_order(nodes=[i.id for i in workflow_data.nodes])
+        workflow_writer.align_node_dimensions(columns)
+        workflow_writer.fold_unimportant_node()
+        pos_caculator.modify_node_layout(columns, align=NodeOptions.calculator_align, method=NodeOptions.layout_calculator)
+        group_caculator.modify_group_layout(orig_groups)
+        if NodeOptions.set_color_for_main_path:
+            main_branch_shader.set_color_for_main_path(list(workflow_reader.id_to_node.keys()))
+        if NodeOptions.remove_nails:
+            workflow_writer.remove_nail()
+        output_filename = "output.json"
         output_path = app.config['temp'] / output_filename
         workflow_writer.export_file(output_path)
-        
         with open(output_path, 'r', encoding='utf-8') as f:
             preview_data = json.load(f)
         return jsonify({
@@ -82,11 +91,12 @@ def generate_layout() -> tuple[Response, Literal[400]] | Response | tuple[Respon
 
 @app.route('/download/<filename>')
 def download_file(filename: str) -> tuple[Response, Literal[404]] | Response:
-    output_path: Path = app.config['temp'] / filename
+    output_path: Path = app.config['temp'] / "output.json"
     if not output_path.exists():
         return jsonify({'error': 'File not found'}), 404
     return send_file(output_path, as_attachment=True)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
+
